98951b6028322a446fb8e89c7bfddbe037054ef2
NickBodmer <bodmerr@yandex.ru>
2019-09-19 11:45:28 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         for (; ; ) {
             int random = (int) (Math.random() * 9);
             System.out.println("Задача угадать число за три попытки");
-            for (int count = 0; ; count++) {
+            for (int count = 1; ; count++) {
                 System.out.println("Введите число ");
                 int a = scanner.nextInt();
                 if (a == random) {
@@ -14,11 +14,11 @@ public class Main {
                     break;
                 } else if (a < random) {
 
-                    System.out.println("Загаданное число больше Попытка: " + count +  " Число " +  random);
+                    System.out.println("Загаданное число больше Попытка: " + count);
                 } else if (a > random) {
-                    System.out.println("Загаданное число меньше Попытка: " + count +  " Число" +  random);
+                    System.out.println("Загаданное число меньше Попытка: " + count);
                 }
-                if (count == 3) {
+                if (count > 3) {
                     System.out.println("Количество попыток истекло, вы проиграли");
                     break;
 
