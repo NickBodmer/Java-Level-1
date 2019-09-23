fd55ae8e134a58462bea8207b060c48135c0c100
NickBodmer <bodmerr@yandex.ru>
2019-09-23 22:20:37 +0300

    1

---
+++ src/Worker.java
@@ -0,0 +1,10 @@
+public class Worker {
+    private String name;
+    private String position;
+    private String email;
+    private int phone;
+    private int zarplata;
+    private int salary;
+    private int age;
+
+}
