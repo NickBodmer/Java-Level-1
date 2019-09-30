379e5b6432935cfa219992f2c49c68a8124c3a33
NickBodmer <bodmerr@yandex.ru>
2019-09-30 10:37:33 +0300

    2

---
+++ src/Animal.java
@@ -0,0 +1,27 @@
+
+public class Animal {
+    String name;
+    float run_max_distance;
+    float swim_max_distance;
+    float jump_max_distance;
+
+    public Animal(String name, float run_max_distance, float swim_max_distance, float jump_max_distance) {
+        this.name = name;
+        this.run_max_distance = run_max_distance;
+        this.swim_max_distance = swim_max_distance;
+        this.jump_max_distance = jump_max_distance;
+    }
+
+    public void run() {
+    }
+
+    public void swim() {
+    }
+
+    public void jump() {
+    }
+
+
+
+}
+
